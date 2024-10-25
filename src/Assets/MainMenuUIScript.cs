7989b30cc3e86a12a0451fc46a0f14375019b349
EwDosbox <144440258+EwDosbox@users.noreply.github.com>
2024-10-25 11:58:55 +0100

    Options Menu added

---
+++ src/Assets/MainMenuUIScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUIScript : MonoBehaviour
+{
+    //Method for quiting the game
+    public void QuitButton_Click()
+    {
+        Application.Quit();
+    }
+}
