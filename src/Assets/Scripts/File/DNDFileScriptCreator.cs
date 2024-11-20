edce2ea511b72937e44574c82c67ab4312c5eaac
EwDosbox <144440258+EwDosbox@users.noreply.github.com>
2024-11-20 17:55:43 +0100

    Generating Scene Started
    1.st Input Field
    Submit Button
    File Creating Script
    Unity 1

---
+++ src/Assets/Scripts/File/DNDFileScriptCreator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNDFileScriptCreator : MonoBehaviour
+{
+    public void CreateFile(string seed)
+    {
+        Debug.Log(seed);
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ src/Assets/Scripts/UI/CreatingUIScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CreatingUIScript : MonoBehaviour
+{
+    private DNDFileScriptCreator dNDFileScriptCreator;
+    private GameObject inputs;
+    private void Awake()
+    {
+        GameObject creator = GameObject.Find("Creator");
+        inputs = GameObject.Find("Inputs");
+
+        //Gets the instnace of the file script
+        dNDFileScriptCreator = creator.GetComponent<DNDFileScriptCreator>();
+    }
+    /// <summary>
+    /// Submits the contensts of the Inputs and gives them to the creator
+    /// </summary>
+    public void SubmitButton_Click()
+    {
+
+        #region Getting Inputs
+
+        string seed = GetInput("Seed") ;
+
+        #endregion
+        //Creates the DND File
+        dNDFileScriptCreator.CreateFile(seed);
+    }
+
+    private string GetInput(string ToFind)
+    {
+        GameObject ToFindGO = inputs.transform.Find(ToFind + "Input").gameObject;
+        GameObject ToFindInputGO = ToFindGO.transform.Find(ToFind +"InputField").gameObject;
+        TMP_InputField ToFindTMPInputField = ToFindInputGO.GetComponent<TMP_InputField>();
+        return ToFindTMPInputField.text;
+    }
+}
