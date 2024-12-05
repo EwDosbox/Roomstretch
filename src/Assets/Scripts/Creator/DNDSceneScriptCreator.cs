eae74eeb55fe9cb9e01bad956b451fcee4a3dbe6
EwDosbox <144440258+EwDosbox@users.noreply.github.com>
2024-12-05 08:27:13 +0100

    Started on DND Scene Script

--- src/Assets/Scripts/Creator/DNDSceneScriptCreator.cs
+++ src/Assets/Scripts/Creator/DNDSceneScriptCreator.cs
@@ -5,5 +5,28 @@ using UnityEngine;
 
 public class DNDSceneScriptCreator : MonoBehaviour
 {
+    public UtilityScript utilityScript;
+    private string dNDFilePath = "";
+
+    private void Awake()
+    {
+
+        dNDFilePath = utilityScript.filePath;
+
+        if(string.IsNullOrEmpty(dNDFilePath))
+        {
+            Debug.Log("Creator: Badly made DND File Path");
+        }
+        else
+        {
+            
+        }
+    }
+    #region Reading Lines
+    private string ReadLine()
+    {
+        return " ";
+    }
+    #endregion
     
 }
